6f0dbeb17705705568d9104557f521e79568e6ab
Tom <zuckm709@gmail.com>
2026-02-16 20:49:36 -0600

    ........

--- fl_skr/auth.py
+++ fl_skr/auth.py
@@ -12,7 +12,6 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
-
     if user_id is None:
         g.user = None
     else:
@@ -28,12 +27,10 @@ def register():
         password = request.form['password']
         db = get_db()
         error = None
-
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-
         if error is None:
             try:
                 db.execute(
@@ -45,9 +42,7 @@ def register():
                 error = f"User {username} is already registered."
             else:
                 return redirect(url_for("auth.login"))
-
         flash(error)
-
     return render_template('auth/register.html')
 
 
@@ -61,19 +56,15 @@ def login():
         user = db.execute(
             'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
-
         if user is None:
             error = 'Incorrect username.'
         elif not check_password_hash(user['password'], password):
             error = 'Incorrect password.'
-
         if error is None:
             session.clear()
             session['user_id'] = user['id']
             return redirect(url_for('index'))
-
         flash(error)
-
     return render_template('auth/login.html')
 
 
